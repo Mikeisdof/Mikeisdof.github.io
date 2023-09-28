e8652d8a802ff0b269fb8f9fadb751a43824df3c
Mikeisdof <145008360+Mikeisdof@users.noreply.github.com>
2023-09-28 12:52:40 +0200

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+let zBereich;
+
+
+function setup() {
+ zBereich = createCanvas(windowWidth, windowHeight);
+  
+ zBereich.position(0,0);
+  zBereich.style('z-index','-1');
+}
+
+function draw() {
+  noStroke();
+  fill(mouseX,mouseY,0);
+  circle(mouseX,mouseY,24);
+  
+}
+
+function mousePressed() {
+  
+  background(0);
+  
+}
